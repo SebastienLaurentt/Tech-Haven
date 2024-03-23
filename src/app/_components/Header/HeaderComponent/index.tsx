c97851e03c885302b38ed558a8697511721980e3
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-23 23:45:04 +0100

    update HeaderComponent with logo

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,5 +1,17 @@
-const HeaderComponent = () => {
-  return <div>HeaderComponent</div>
+import Link from 'next/link'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return (
+    <nav>
+      <Gutter>
+        <Link href="/">
+          <img src="/logo-black.svg" alt="logo" />
+        </Link>
+      </Gutter>
+    </nav>
+  )
 }
 
 export default HeaderComponent
