b379eb4966ee2ee3fbf79b73f33b3d7f6b0076a2
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-24 13:03:34 +0100

    add FooterComponent second part

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -7,8 +7,11 @@ import { Gutter } from '../../Gutter'
 
 import classes from './index.module.scss'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '../../Button'
+import { Footer } from '../../../../payload/payload-types'
 
-const FooterComponent = () => {
+const FooterComponent = ({ footer }: { footer: Footer }) => {
   const pathname = usePathname()
 
   return (
@@ -32,6 +35,41 @@ const FooterComponent = () => {
         </ul>
       </Gutter>
 
+      <div className={classes.footer}>
+        <Gutter>
+          <div className={classes.wrap}>
+            <Link href="/">
+              <Image src="/logo-white.svg" alt="logo" width={170} height={50} />
+            </Link>
+
+            <p>{footer?.copyright}</p>
+
+            <div className={classes.socialLinks}>
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className={classes.socialLinkItem}
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className={classes.socialIcon}
+                    />
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        </Gutter>
+      </div>
     </footer>
   )
 }
