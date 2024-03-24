b59130eadcc5d072e186cd7877b61634a6568f08
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-24 12:56:23 +0100

    import use client and ul

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,16 +1,36 @@
+'use client'
+
 import { usePathname } from 'next/navigation'
 
-import { noHeaderFooterUrls } from '../../../constants'
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
 import { Gutter } from '../../Gutter'
 
 import classes from './index.module.scss'
+import Image from 'next/image'
 
 const FooterComponent = () => {
   const pathname = usePathname()
 
   return (
     <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ''}>
-      <Gutter></Gutter>
+      <Gutter>
+        <ul className={classes.inclusions}>
+          {inclusions.map(inclusion => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={36}
+                height={36}
+                className={classes.icon}
+              />
+
+              <h5 className={classes.title}>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
     </footer>
   )
 }
