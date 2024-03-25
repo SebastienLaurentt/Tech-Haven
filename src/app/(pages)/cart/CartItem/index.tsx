3868b6418df42fa1e2d32e7c9657c316479011af
SebastienLaurentt <slaurent.26@gmail.com>
2024-03-25 09:06:07 +0100

    update CartPage component with CartItem component

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,7 +1,11 @@
 import classes from './index.module.scss'
 
 const CartItem = () => {
-  return <li className={classes.item}>CartItem</li>
+  return (
+    <li className={classes.item}>
+      <h6>TITLE</h6>
+    </li>
+  )
 }
 
 export default CartItem
